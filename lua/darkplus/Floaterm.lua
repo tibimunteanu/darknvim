a1ce8ec5695dd5995e9d6da82adb40ab902a570d
Tibi Munteanu <sn_tibi@yahoo.com>
2021-12-30 16:27:00 +0200

    first commit

---
+++ lua/darkplus/Floaterm.lua
@@ -0,0 +1,6 @@
+local Floaterm = {
+	Floaterm = { bg = C.bg },
+	FloatermBorder = { fg = C.gray, bg = C.bg },
+}
+
+return Floaterm
---
+++ lua/darkplus/Telescope.lua
@@ -0,0 +1,8 @@
+local Telescope = {
+	TelescopeSelection = { fg = C.hint_blue },
+	TelescopeMatching = { fg = C.info_yellow, style = "bold" },
+	TelescopeBorder = { fg = C.gray, bg = C.bg },
+}
+
+return Telescope
+
